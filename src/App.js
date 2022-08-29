afdd30844508db351b6e56ded1cec1cd1bb00485
stardust6653 <stardust6653@gmail.com>
2022-08-29 18:38:34 +0900

    feat: useState와 onClick 이벤트를 활용한 result 값 출력

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import "./App.css";
 
 function App() {
   const [keyword, setKeyword] = useState("");
+  const [result, setResult] = useState("");
 
   function handleOnChange(event) {
     setKeyword(event.target.value);
+  }
+
+  function handleOnClick(event) {
+    event.preventDefault();
+    setResult(`지금 당신이 검색한 검색어는 ${keyword}`);
   }
 
   return (
     <div>
       <form>
         <input onChange={handleOnChange}></input>
-        <button>Search</button>
+        <button onClick={handleOnClick}>Search</button>
       </form>
       <p>{keyword}</p>
+      <p>{result}</p>
     </div>
   );
 }
