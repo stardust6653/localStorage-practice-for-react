ec1fe7c7a2dbb7a6d7ebddcc2d22ba1d46060935
stardust6653 <stardust6653@gmail.com>
2022-08-29 18:50:11 +0900

    feat: 로컬스토리지에 저장되어 있는 값을 새로고침해도 출력되게 하기

--- src/App.js
+++ src/App.js
@@ -2,21 +2,34 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [keyword, setKeyword] = useState("");
+  const [keyword, setKeyword] = useState(() => {
+    return window.localStorage.getItem("keyword");
+  });
   const [result, setResult] = useState("");
+  const [typing, setTyping] = useState(false);
 
   function handleOnChange(event) {
     setKeyword(event.target.value);
+    setTyping(true);
   }
 
   function handleOnClick(event) {
     event.preventDefault();
     setResult(`지금 당신이 검색한 검색어는 ${keyword}`);
+    setTyping(false);
   }
 
   useEffect(() => {
     window.localStorage.setItem("keyword", keyword);
   }, [keyword]);
+
+  useEffect(() => {
+    window.localStorage.setItem("result", result);
+  }, [keyword]);
+
+  useEffect(() => {
+    window.localStorage.setItem("typing", typing);
+  }, [keyword, result]);
 
   return (
     <div>
